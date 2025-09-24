0774663f73d3562d280cc55287adccc8b60964ce
[dyad] <l21d0v1c.c1v0d21l@gmail.com>
2025-09-24 23:22:18 +0200

    [dyad] Ajustement de la couleur de fond et du texte de la page d'accueil - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,9 +7,8 @@ const IndexPage = () => {
       className="min-h-screen flex flex-col items-center justify-center p-4 bg-cover bg-center relative"
       style={{ backgroundImage: `url('/ancient_white_magical_door.png')` }}
     >
-      {/* Overlay pour améliorer la lisibilité du texte sur l'image */}
-      <div className="absolute inset-0 bg-black opacity-50"></div> 
-      <h1 className="text-4xl font-bold text-gray-100 mb-8 relative z-10">
+      {/* Suppression de l'overlay pour un fond plus clair */}
+      <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-100 mb-8 relative z-10">
         Room of Requirement
       </h1>
       <div className="relative z-10">
