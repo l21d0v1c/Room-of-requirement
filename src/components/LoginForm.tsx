5bc6c8739ca86ccc79561a2a9d2e6293774a0fef
[dyad] <l21d0v1c.c1v0d21l@gmail.com>
2025-09-24 23:26:26 +0200

    [dyad] Ajustement de la couleur des champs de saisie du formulaire - wrote 1 file(s)

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -94,7 +94,7 @@ const LoginForm = () => {
               <FormItem>
                 <FormLabel className="text-gray-100 text-center w-full block">Thing</FormLabel>
                 <FormControl>
-                  <Input {...field} />
+                  <Input className="bg-gray-800 text-gray-100 border-gray-700" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -107,7 +107,7 @@ const LoginForm = () => {
               <FormItem>
                 <FormLabel className="text-gray-100 text-center w-full block">Magic word</FormLabel>
                 <FormControl>
-                  <Input type="password" {...field} />
+                  <Input type="password" className="bg-gray-800 text-gray-100 border-gray-700" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
