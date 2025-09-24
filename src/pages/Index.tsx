e9e2ed7d2146eae95a1fb644e68345204f81518d
[dyad] <l21d0v1c.c1v0d21l@gmail.com>
2025-09-24 23:20:30 +0200

    [dyad] Ajout d'une image de fond à la page d'accueil - wrote 2 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,11 +3,18 @@ import LoginForm from '@/components/LoginForm';
 
 const IndexPage = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900 p-4">
-      <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-100 mb-8">
+    <div 
+      className="min-h-screen flex flex-col items-center justify-center p-4 bg-cover bg-center relative"
+      style={{ backgroundImage: `url('/ancient_white_magical_door.png')` }}
+    >
+      {/* Overlay pour améliorer la lisibilité du texte sur l'image */}
+      <div className="absolute inset-0 bg-black opacity-50"></div> 
+      <h1 className="text-4xl font-bold text-gray-100 mb-8 relative z-10">
         Room of Requirement
       </h1>
-      <LoginForm />
+      <div className="relative z-10">
+        <LoginForm />
+      </div>
     </div>
   );
 };
