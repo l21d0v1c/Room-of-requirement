8ceed8d118630ad82b6204dad4fc35a4f4847d24
[dyad] <l21d0v1c.c1v0d21l@gmail.com>
2025-09-25 00:08:49 +0200

    Reverted all changes back to version 3009459eb1aeb52f9d72ccdbe148b4d1a7af55e5

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import LoginForm from '@/components/LoginForm';
-import WelcomeDialog from '@/components/WelcomeDialog'; // Importez le nouveau composant
 
 const IndexPage = () => {
   return (
@@ -8,13 +7,13 @@ const IndexPage = () => {
       className="min-h-screen flex flex-col items-center justify-center p-4 bg-cover bg-center relative"
       style={{ backgroundImage: `url('/ancient_white_magical_door.png')` }}
     >
+      {/* Suppression de l'overlay pour un fond plus clair */}
       <h1 className="text-4xl font-bold text-gray-100 mb-8 relative z-10">
         Room of Requirement
       </h1>
       <div className="relative z-10">
         <LoginForm />
       </div>
-      <WelcomeDialog /> {/* Ajout du composant WelcomeDialog ici */}
     </div>
   );
 };
