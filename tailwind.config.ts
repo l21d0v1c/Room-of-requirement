08476a6dcafb63fcdb1e142807d1a6d5b5182f97
[dyad] <l21d0v1c.c1v0d21l@gmail.com>
2025-09-24 23:35:58 +0200

    [dyad] Application de la police Inconsolata à l'ensemble de l'application - wrote 3 file(s)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ export default {
       },
     },
     extend: {
+      fontFamily: { // Ajout de la famille de polices
+        inconsolata: ['Inconsolata', 'monospace'],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
